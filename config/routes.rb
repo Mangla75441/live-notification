f8ab1e3088fc3b7d40c4685adadc452e5a017fbd
Mangla75441 <manglaverma0@gmail.com>
2018-06-10 12:12:36 +0530

    my first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  
+  devise_scope :user do
+  resources :home, only: [:index]	
+  	
+  root to: "devise/sessions#new"
+  end
+
+  devise_for :users, controllers: { registrations: "registrations" }
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
